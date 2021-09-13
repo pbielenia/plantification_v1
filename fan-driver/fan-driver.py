fcf9e67724f6d26c4cb04c2c28aa770175597591
Piotr Bielenia <piotrbielenia0@gmail.com>
2021-09-13 19:26:16 +0200

    BRANCH-v1.1: refactor fan-driver-py

--- fan-driver/fan-driver.py
+++ fan-driver/fan-driver.py
@@ -1,5 +1,5 @@
 import pigpio
-import datetime 
+import datetime
 import time
 import enum
 import sys
@@ -8,12 +8,15 @@ import paho.mqtt.client as mqtt
 
 
 class FanDriver:
-    def __init__(self, fan_id, gpio_pin):
-        self.fan_id = fan_id
+    def __init__(self, name, gpio_pin):
+        self.name = name
         self.gpio_pin = gpio_pin
 
-        self.control_topic = 'plant/fan/{}/control'.format(fan_name)
-        self.status_topic = 'plant/fan/{}/status'.format(fan_name)
+        self.control_topic = 'plant/fan/{}/control'.format(self.name)
+        self.status_topic = 'plant/fan/{}/status'.format(self.name)
+
+        self.control_enable_command = 'ENABLE'
+        self.control_disable_command = 'DISABLE'
 
         self.mqtt_client = mqtt.Client()
         self.pi = pigpio.pi()
@@ -27,12 +30,15 @@ class FanDriver:
     def start(self):
         self.mqtt_client.connect('localhost')
         self.mqtt_client.subscribe(self.control_topic)
-        self.mqtt_client.message_callback_add(self.control_topic, self.on_control_topic_message)
+        self.mqtt_client.message_callback_add(
+            self.control_topic, self.on_control_topic_message)
 
         if not self.pi.connected:
             raise Exception('Cannot connect to GPIO')
+
         self.pi.set_mode(self.gpio_pin, pigpio.OUTPUT)
         self.turn_fan_off()
+        self.publish_pin_status()
 
         run = True
         while run:
@@ -40,26 +46,40 @@ class FanDriver:
 
     def on_control_topic_message(self, client, userdata, message):
         payload = message.payload.decode('utf-8')
-        if payload == 'ENABLE':
+        if payload == self.control_enable_command:
             self.turn_fan_on()
-        elif payload == 'DISABLE':
+        elif payload == self.control_disable_command:
             self.turn_fan_off()
         else:
             print('Unknown message:', payload)
+            self.publish_status_message(
+                'Unknown message: {}. Available commands are: {}, {}.'
+                .format(payload,
+                        self.control_enable_command,
+                        self.control_disable_command))
+
+        self.publish_pin_status()
 
     def turn_fan_on(self):
         self.pi.write(self.gpio_pin, 1)
-        self.mqtt_client.publish(self.status_topic, 'ENABLED')
 
     def turn_fan_off(self):
         self.pi.write(self.gpio_pin, 0)
-        self.mqtt_client.publish(self.status_topic, 'DISABLED')
+
+    def publish_status_message(self, message):
+        self.mqtt_client.publish(self.status_topic, message)
+
+    def publish_pin_status(self):
+        pin_status = 'enabled' if self.pi.read(self.gpio_pin) else 'disabled'
+        message = 'The fan on the pin {} has been {}.'.format(
+            self.gpio_pin, pin_status)
+        self.publish_status_message(message)
 
 
 if __name__ == "__main__":
 
     if len(sys.argv) != 3:
-        print('Usage: python3 fan-driver.py <name> <on/off pin>')
+        print('Usage: python3 fan-driver.py <fan-name> <gpio-pin>')
         exit(1)
 
     fan_name = sys.argv[1]
@@ -67,4 +87,3 @@ if __name__ == "__main__":
 
     with FanDriver(fan_name, gpio_pin) as fan_driver:
         fan_driver.start()
-
