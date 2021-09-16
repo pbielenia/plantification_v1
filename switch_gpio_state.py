fe92d207b1ca2a40f9db949dff3b11132cb062f6
Piotr Bielenia <piotrbielenia0@gmail.com>
2021-09-16 03:10:35 +0200

    BRANCH-v1.1: prepare a first version of switch_gpio_state.py script

---
+++ switch_gpio_state.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+import paho.mqtt.client as mqtt
+import json
+import sys
+
+
+class ConfigProvider:
+    def __init__(self, console_params, file_path):
+        self.device_type = console_params[1]
+        self.device_id = int(console_params[2])
+        self.action = console_params[3]
+
+        if self.action != 'on' or self.action != 'off':
+            raise Exception('Unknown action: {}'.format(self.action))
+
+        config_file = open(file_path, 'r')
+        self._file_params = json.loads(config_file.read())
+
+        self.control_topic = self._get_topic('controlTopic')
+
+    def _get_topic(self, json_key):
+        device_type_placeholder = '<device-type>'
+        device_id_placeholder = '<device-id>'
+
+        if json_key in self._file_params:
+            topic_scheme = self._file_params[json_key]
+            if device_type_placeholder in topic_scheme and device_id_placeholder in topic_scheme:
+                return topic_scheme.replace(
+                    device_type_placeholder, self.device_type).replace(device_id_placeholder, str(self.device_id))
+            else:
+                raise Exception('Missing \"{}\" or \"{}\" placeholder in the topic scheme: \"{}\".'.format(
+                    device_type_placeholder, device_id_placeholder, topic_scheme))
+        else:
+            raise Exception(
+                'Key \"{}\" not found in the config file!'.format(json_key))
+
+
+def print_usage():
+    print('Usage: python3 {} <device-type> <device-id> <action:on|off>'
+          .format(sys.argv[0]))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        if len(sys.argv == 2) and sys.argv[1] == '--help':
+            print_usage()
+            exit(0)
+        else:
+            print_usage()
+            exit(2)
+
+    try:
+        config_provider = ConfigProvider(
+            sys.argv, 'mqtt-gpio-switcher/config.json')
+
+        message = None
+        if config_provider.action == 'on':
+            message = 'ENABLE'
+        elif config_provider.action == 'off':
+            message = 'DISABLE'
+
+        mqtt_client = mqtt.Client()
+        mqtt_client.connect('localhost')
+        mqtt_client.publish(config_provider.status_topic, message)
+
+    except Exception as e:
+        print('Something went wrong:\n\t', e)
+        exit(1)
